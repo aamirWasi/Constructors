a1226364373f5b03e0ac0cebd349ba90b53b3c2d
aamir <wasi3aamir@gmail.com>
2019-01-27 13:43:38 +0600

    Practicing Constructors properties in c#

---
+++ ConsoleApp1/ConsoleApp1/Animal.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Animal
+    {
+        private string name;
+        private string sound;
+
+        public const string Shelter = "Derek's Home for Animal";
+        public readonly int IdNum;
+        public void MakeSound()
+        {
+            Console.WriteLine($"{name} says {sound}");
+        }
+
+        public Animal()
+            : this("No Name", "No Sound") { }
+        public Animal(string name)
+            :this(name,"No Sound") { }
+        public Animal(string name,string sound)
+        {
+            SetName(name);
+            Sound = sound;
+            NumofAnimals = 1;
+            Random rnd = new Random();
+            IdNum = rnd.Next(1, 2147483640);
+
+        }
+        public void SetName(string name)
+        {
+            if (!name.Any(char.IsDigit))
+            {
+                this.name = name;
+            }
+            else
+            {
+                this.name = "No Name";
+                Console.WriteLine("Name can't contain numbers");
+            }
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public string Sound
+        {
+            get { return sound; }
+            set
+            {
+                if(value.Length > 10)
+                {
+                    sound = "No Sound";
+                    Console.WriteLine("Sound is too Long");
+                }
+                sound = value;
+            }
+        }
+        public string Owner { get; set; } = "No Owner";
+
+        public static int numOfAnimals = 0;
+        public static int NumofAnimals
+        {
+            get { return numOfAnimals; }
+            set { numOfAnimals += value; }
+        }
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cat = new Animal();
+            cat.SetName("Whiskers");
+            cat.Sound = "Meow";
+            Console.WriteLine("The cat named is {0} & says {1}",
+                cat.GetName(),
+                cat.Sound);
+            cat.Owner = "Derek";
+            Console.WriteLine("{0}'s owner is {1}",
+                cat.GetName(),
+                cat.Owner);
+
+            Console.WriteLine("{0}'s Shelter Id is {1}",
+                cat.GetName(),
+                cat.IdNum);
+
+            Console.WriteLine("# of Animals {0}",
+                Animal.NumofAnimals);
+            Animal fox = new Animal("Oscar", "Rawwr");
+            Console.WriteLine("# of Animals {0}",
+                Animal.NumofAnimals);
+        }
+    }
+}
